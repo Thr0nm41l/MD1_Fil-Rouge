d59b61c74339e9a9fa0d78a7a38485ea504910e8
Thr0nm41l <thronm41l@gmail.com>
2026-05-08 18:47:08 +0200

    Added treafik to load balance trafic

--- helmcharts/stop_infra.sh
+++ helmcharts/stop_infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-stacknumber=7
+stacknumber=9
 stopedstacknumber=0
 
 # Kill any running port-forward processes
@@ -8,6 +8,20 @@ echo "Stopping port-forwards..." >&2
 #pkill -f "kubectl port-forward.*airflow"
 #pkill -f "kubectl port-forward.*pgadmin"
 echo "Port-forwards stopped." >&2
+
+kubectl delete -f ingress.yaml 2>/dev/null
+if [ $? -ne 0 ]; then
+  echo "Failed to delete Ingress rules"
+  else
+    stopedstacknumber=$((stopedstacknumber + 1))
+fi
+
+helm uninstall traefik -n traefik
+if [ $? -ne 0 ]; then
+  echo "Failed to uninstall Traefik"
+  else
+    stopedstacknumber=$((stopedstacknumber + 1))
+fi
 
 kubectl delete -f mkdocs-deployment.yaml 2>/dev/null
 if [ $? -ne 0 ]; then
